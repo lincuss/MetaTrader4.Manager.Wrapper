feff315bb5a40d59bf2d75ee965200296186d2ce
Nikita Gusev <nikita.gusev.1987@gmail.com>
2016-08-15 20:27:22 +0200

    Fixed memory problems in AdministratorDatabaseCommands

--- src/P23.MetaTrader4.Manager.ClrWrapper/AdministratorDatabaseCommands.cpp
+++ src/P23.MetaTrader4.Manager.ClrWrapper/AdministratorDatabaseCommands.cpp
@@ -17,6 +17,7 @@ IList<P23::MetaTrader4::Manager::Contracts::UserRecord^>^  P23::MetaTrader4::Man
 		result->Add(Convert(&users[i]));
 	
 	_manager->Manager->MemFree(users);
+	Marshal::FreeHGlobal(IntPtr(g));
 
 	return result;
 }
@@ -36,6 +37,7 @@ IList<P23::MetaTrader4::Manager::Contracts::TradeRecord^>^ P23::MetaTrader4::Man
 		result->Add(Convert(&trades[i]));
 
 	_manager->Manager->MemFree(trades);
+	Marshal::FreeHGlobal(IntPtr(g));
 
 	return result;
 }
@@ -46,7 +48,12 @@ int P23::MetaTrader4::Manager::ClrWrapper::AdmBalanceFix(IList<int>^ logins)
 	int* l = new int[total];
 	for (int i = 0; i < total; i++)
 		l[i] = logins[i];
-	return _manager->Manager->AdmBalanceFix(l, total);
+
+	int result = _manager->Manager->AdmBalanceFix(l, total);
+
+	delete[] l;
+
+	return result;
 }
 
 int P23::MetaTrader4::Manager::ClrWrapper::AdmTradesDelete(IList<int>^ orders)
@@ -55,12 +62,23 @@ int P23::MetaTrader4::Manager::ClrWrapper::AdmTradesDelete(IList<int>^ orders)
 	int* l = new int[total];
 	for (int i = 0; i < total; i++)
 		l[i] = orders[i];
-	return _manager->Manager->AdmTradesDelete(l, total);
+
+	int result = _manager->Manager->AdmTradesDelete(l, total);
+
+	delete[] l;
+
+	return result
 }
 
 int P23::MetaTrader4::Manager::ClrWrapper::AdmTradeRecordModify(P23::MetaTrader4::Manager::Contracts::TradeRecord^ trade)
 {	
-	return _manager->Manager->AdmTradeRecordModify(Convert(trade));
+	TradeRecord* tradeRecord = Convert(trade);
+
+	int result = _manager->Manager->AdmTradeRecordModify(tradeRecord);
+
+	delete tradeRecord;
+
+	return result
 }
 
 IList<P23::MetaTrader4::Manager::Contracts::BalanceDiff^>^ P23::MetaTrader4::Manager::ClrWrapper::AdmBalanceCheck(IList<int>^ logins)
@@ -69,12 +87,15 @@ IList<P23::MetaTrader4::Manager::Contracts::BalanceDiff^>^ P23::MetaTrader4::Man
 	int* l = new int[logins->Count];
 	for (int i = 0; i < logins->Count; i++)
 		l[i] = logins[i];
+
 	BalanceDiff* balanceDiff = _manager->Manager->AdmBalanceCheck(l, &total);
 	IList<P23::MetaTrader4::Manager::Contracts::BalanceDiff^>^ output = gcnew List<P23::MetaTrader4::Manager::Contracts::BalanceDiff^>();
 	for (int i = 0; i < total; i++)
 		output->Add(Convert(&balanceDiff[i]));
 
 	_manager->Manager->MemFree(balanceDiff);
+	delete[] l;
+	delete balanceDiff;
 
 	return output;
 }
